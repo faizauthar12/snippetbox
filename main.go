4a4a37cb17aab29a9a64ea20d303e13466c7651e
Faiz Authar <faizauthar@gmail.com>
2023-01-12 15:20:52 +0700

    let user know /snippet/create only allow POST

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func snippetView(w http.ResponseWriter, r *http.Request)  {
 func snippetCreate(w http.ResponseWriter, r *http.Request)  {
     // check method != POST
     if r.Method != "POST" {
+        w.Header().Set("Allow", "POST")
         w.WriteHeader(405)
         w.Write([]byte("Method Not Allowed"))
     }
