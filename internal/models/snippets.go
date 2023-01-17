3d50088db2136987e9394f0c5b85fbceee920fa0
Faiz Authar <faizauthar@gmail.com>
2023-01-17 15:02:37 +0700

    Executing SQL Statement in insert function

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -22,7 +22,26 @@ type SnippetModel struct {
 
 // This will insert a new snippet into database
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-    return 0, nil
+    // SQL Statement
+    stmt := `INSERT INTO snippets (title, content, created, expires)
+    VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+    // use Exec to insert to the DB
+    result, err := m.DB.Exec(stmt, title, content, expires)
+    if err != nil {
+        return 0, err
+    } 
+
+    // Use the LastInsertId() method on the result to get the ID of our
+    // newly inserted record in the snippets table.
+    id, err := result.LastInsertId()
+    if err != nil {
+        return 0, err
+    }
+
+    // The ID returned has the type int64, so we convert it to an int type
+    // before returning
+    return int(id), nil
 }
 
 // This will return a specific snippet based on tis id
