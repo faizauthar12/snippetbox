c33a12abcfc107bcd882e12e71b720cd0e85a085
Faiz Authar <faizauthar@gmail.com>
2023-01-31 15:42:05 +0700

    Displaying dynamic data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
 
@@ -24,23 +25,6 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
     for _, snippet := range snippets {
         fmt.Fprintf(w, "%+v\n", snippet)
     }
-
-    //files := []string{
-    //    "./ui/html/base.html",
-    //    "./ui/html/partials/nav.html",
-    //    "./ui/html/pages/home.html",
-    //}
-    
-    //ts, err := template.ParseFiles(files...)
-    //if err != nil {
-    //    app.serverError(w, err)
-    //    return
-    //}
-
-    //err = ts.ExecuteTemplate(w, "base", nil)
-    //if err != nil {
-    //    app.serverError(w, err)
-    //}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -60,7 +44,27 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    fmt.Fprintf(w, "%+v", snippet)
+    files := []string{
+        "./ui/html/base.html",
+        "./ui/html/partials/nav.html",
+        "./ui/html/pages/view.html",
+    }
+
+    // Parsing the template
+    ts, err := template.ParseFiles(files...)
+    if err != nil {
+        app.serverError(w,err)
+        return
+    }
+
+    data := &templateData{
+        Snippet: snippet,
+    }
+
+    err = ts.ExecuteTemplate(w, "base", data)
+    if err != nil {
+        app.serverError(w,err)
+    }
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "snippetbox.faizauthar12.github.io/internal/models"
+
+type templateData struct {
+    Snippet *models.Snippet
+}
