b4c118b63b605c2e76c3b1efaa956b2b79374f9f
Faiz Authar <faizauthar@gmail.com>
2023-02-11 20:31:19 +0700

    dynamically changes footer year

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -20,10 +20,11 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
         app.serverError(w, err)
         return
     }
+
+    data := app.newTemplateData(r)
+    data.Snippets = snippets
     
-    app.render(w, http.StatusOK, "home.tmpl", &templateData{
-        Snippets: snippets,
-    })
+    app.render(w, http.StatusOK, "home.tmpl", data)
 }
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
@@ -42,10 +43,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
         }
         return
     }
+
+    data := app.newTemplateData(r)
+    data.Snippet = snippet
     
-    app.render(w, http.StatusOK, "view.tmpl", &templateData{
-        Snippet: snippet,
-    })
+    app.render(w, http.StatusOK, "view.tmpl", data)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
