ea9388918ce27f4776b3bf28d7ac13c91a87bee6
Faiz Authar <faizauthar@gmail.com>
2023-02-16 08:23:48 +0700

    createPost: Add validation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/julienschmidt/httprouter"
 	"snippetbox.faizauthar12.github.io/internal/models"
@@ -59,6 +61,7 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	err := r.ParseForm()
 	if err != nil {
 		app.clienError(w, http.StatusBadRequest)
+		return
 	}
 
 	title := r.PostForm.Get("title")
@@ -67,7 +70,31 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clienError(w, http.StatusBadRequest)
+		return
 	}
+
+	// Validation
+	fieldErrors := make(map[string]string)
+
+	if strings.TrimSpace(title) == "" {
+		fieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(title) > 100 {
+		fieldErrors[title] = "This field cannot be more than 100 characters long"
+	}
+
+	if strings.TrimSpace(content) == "" {
+		fieldErrors["content"] = "This field cannot be blank"
+	}
+
+	if expires != 1 && expires != 7 && expires != 365 {
+		fieldErrors["expires"] = "This field must equal 1, 7, 365"
+	}
+
+	if len(fieldErrors) > 0 {
+		fmt.Fprint(w, fieldErrors)
+		return
+	}
+
 
 	id, err := app.snippets.Insert(title, content, expires)
 	if err != nil {
